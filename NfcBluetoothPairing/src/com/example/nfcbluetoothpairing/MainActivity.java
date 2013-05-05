033ac58a8fa9078781530bd0d941d3abcff7d791
Armin Wieser <armin.wieser@gmail.com>
2013-05-05 13:59:44 +0200

    update Signed-off-by: Armin Wieser <armin.wieser@gmail.com>

--- NfcBluetoothPairing/src/com/example/nfcbluetoothpairing/MainActivity.java
+++ NfcBluetoothPairing/src/com/example/nfcbluetoothpairing/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.nfcbluetoothpairing;
 
-import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+
 import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -19,7 +18,6 @@ public class MainActivity extends Activity implements CreateNdefMessageCallback 
 
 	private static final String NFC_BLUETOOTH_PAIRING = "nfc-bluetooth-pairing";
 	private NfcAdapter mNfcAdapter;
-	private PendingIntent pendingIntent;
 	private TextView textView;
 
 	@Override
@@ -29,9 +27,6 @@ public class MainActivity extends Activity implements CreateNdefMessageCallback 
         textView = (TextView) findViewById(R.id.textView);
 		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		mNfcAdapter.setNdefPushMessageCallback(this, this);
-
-		/*pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
-				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);*/
 	}
 
 	@Override
@@ -47,24 +42,15 @@ public class MainActivity extends Activity implements CreateNdefMessageCallback 
 		 setIntent(intent);
 	}
 
-	protected void onResume() {
-		Log.d(NFC_BLUETOOTH_PAIRING, "onResume()");
+	@Override
+	public void onResume() {
+		Log.d(NFC_BLUETOOTH_PAIRING, "onResume()" + getIntent().getAction());
 		super.onResume();
-//		mNfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
-
+		
 		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
 			processIntent(getIntent());
 		}
-
-		
-
 	};
-
-	@Override
-	protected void onPause() {
-//		mNfcAdapter.disableForegroundDispatch(this);
-		super.onPause();
-	}
 
 	private void processIntent(Intent intent) {
 		Log.d(NFC_BLUETOOTH_PAIRING, "processintent: " + intent.getAction());
@@ -81,5 +67,7 @@ public class MainActivity extends Activity implements CreateNdefMessageCallback 
 	public NdefMessage createNdefMessage(NfcEvent event) {
 		return new NdefMessage(new NdefRecord[] {NdefRecord.createMime("application/vnd.nfcbluetooth", "hallo".getBytes())});
 	}
+
+	
 
 }
